cfb84f84f2477a70e966035b6946b154041da02c
RY-Xin <xinhuiyu0216@gmail.com>
2025-10-29 22:19:13 -0400

    Day1: basic cache structure and setup

---
+++ container_demo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+using namespace std;
+
+int main() {
+    unordered_map<int, string> mp; //Create an unordered_map
+    //Insert key-value pairs
+    mp[1] = "apple";
+    mp[2] = "banana";
+    mp[3] = "cherry";
+    mp.erase(2);
+    mp[4] = "pear";
+
+    cout << "unoredered_map contains:" << endl;
+    for (auto &p : mp) {
+        cout << p.first << " -> " << p.second << endl; //Iterate and print all key-value pairs
+    }
+
+    list<int> lst = {1, 2, 3}; //Create a doubly linked list
+    lst.push_front(0); //Insert at the front
+    lst.push_back(4); //Insert at the back
+
+    cout << "\nlist contains:" << endl;
+    for (int x : lst) cout << x << " "; //Traverse and print list elements
+
+    cout << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int &ref = a; // reference, ref is an alias of 'a'
+    int *ptr = &a; //pointer that stores the address of 'a'
+
+    cout << "a = " << a << endl; //print the value of variable 'a'
+    cout << "ref = " << ref << endl; //print the value of 'ref' (same as 'a')
+    cout << "*ptr = " << *ptr << endl; //print the value pointed to by 'ptr'
+
+    ref = 20; //modify 'a' through the 'ref'
+    cout << "After change: a = " << a << endl; //print the updated value of 'a'
+    return 0;
+}
